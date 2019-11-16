074505baab5242143dd77f394c595f66ff0e0468
Yz4230 <yuzupon1480@yahoo.co.jp>
2019-11-17 00:50:56 +0900

    Initial Commit

---
+++ src/modsyncer/Controller.java
@@ -0,0 +1,83 @@
+package modsyncer;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.InputEvent;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.text.Text;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    public Text date_added_TX;
+    public Text full_path_TX;
+    public Text num_of_files_TX;
+    public CheckBox server_mode_CBX;
+    public ListView<String> modList_LV;
+    public TextField modsPass_TF;
+    public TextField ip_address_TF;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        UpdateModFiles();
+        modsPass_TF.setText(Main.MODS_FILEPATH);
+    }
+
+    @FXML
+    protected void onUpdateButtonClick(ActionEvent event) {
+        Main.MODS_FILEPATH = modsPass_TF.getText();
+        modList_LV.getItems().clear();
+        UpdateModFiles();
+    }
+
+    @FXML
+    public void onSyncButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void onSelect(InputEvent inputEvent) {
+        if (inputEvent instanceof KeyEvent && !((KeyEvent) inputEvent).getCode().isArrowKey()) return;
+        String fullPath = Main.MODS_FILEPATH + "\\" + modList_LV.getSelectionModel().getSelectedItem();
+        BasicFileAttributes attributes;
+        try {
+            attributes = Files.readAttributes(Paths.get(fullPath), BasicFileAttributes.class);
+            FileTime time = attributes.creationTime();
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            date_added_TX.setText(dateFormat.format(new Date(time.toMillis())));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        full_path_TX.setText(fullPath);
+    }
+
+    public void onServerModeCheckBoxChanged(ActionEvent event) {
+        if (server_mode_CBX.isSelected())
+            ip_address_TF.setPromptText("[port] here");
+        else
+            ip_address_TF.setPromptText("[IP address]:[port] here");
+    }
+
+    private void UpdateModFiles() {
+        File dir = new File(Main.MODS_FILEPATH);
+        File[] files = dir.listFiles();
+        if (files != null) {
+            Arrays.stream(files).filter(File::isFile).forEach(f -> modList_LV.getItems().add(f.getName()));
+            num_of_files_TX.setText(String.valueOf(files.length));
+        }
+    }
+}
---
+++ src/modsyncer/Main.java
@@ -0,0 +1,61 @@
+package modsyncer;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Main extends Application {
+    public static final String CONFIG_FILEPATH = "./modsyncer_config.properties";
+    public static String MODS_FILEPATH;
+    public static String SERVER_IP;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("update_icon.png")));
+        primaryStage.setTitle("Minecraft Mod Syncer");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+        File configFile = new File(CONFIG_FILEPATH);
+        Properties properties = new Properties();
+        try {
+            InputStream inputStream = new FileInputStream(configFile);
+            properties.load(inputStream);
+            properties.setProperty("server_ip", SERVER_IP);
+            properties.setProperty("mods_folder_path", MODS_FILEPATH);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        File configFile = new File(CONFIG_FILEPATH);
+        Properties properties = new Properties();
+        try {
+            if (!configFile.exists()) configFile.createNewFile();
+            InputStream inputStream = new FileInputStream(configFile);
+            properties.load(inputStream);
+            SERVER_IP = properties.getProperty("server_ip");
+            MODS_FILEPATH = properties.getProperty("mods_folder_path");
+            System.out.println(SERVER_IP);
+            System.out.println(MODS_FILEPATH);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        launch(args);
+    }
+}
